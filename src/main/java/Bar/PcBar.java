f0a242fa8b3bd933d8d5c5a809af2c1b94e5c777
Nathanelisabetinho <Nathanelisabetinho@DESKTOP-4IQBFES>
2023-03-16 03:01:18 -0300

    comit pasta renomeada Pasta Bar adicionada Pasta Biciletas Adicionada

--- src/main/java/Bar/PcBar.java
+++ src/main/java/Bar/PcBar.java
@@ -1,7 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//Criar uma Classe para Cadastrar Pedido, com 5 atributos;
+//Desses 5 atributos pelo menos 1 deverá ser Valor;
+///A Classe deverá conter métodos para definir cada 1 dos 5 atributos;
+
+//Na classe Principal do Solicitar o Cadastro de 5 pedidos; 
+
+//Na Classe Principal Retornar 1 Mensagem destacando o Total dos 5 pedidos;
+
 package Bar;
 
 import java.util.Scanner;
